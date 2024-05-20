713caa1ef425f555e667178f7043c44a845b2bf9
Volodymyr Kolomiiets <valdi6014@gmail.com>
2024-05-20 11:12:27 +0300

    Fixed flake errors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -15,10 +17,13 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark) * (self.average_rating /
-                                          self.distance_from_city_center), 1)
+        return round(car.comfort_class
+                     * (self.clean_power
+                        - car.clean_mark)
+                     * (self.average_rating
+                        / self.distance_from_city_center), 1)
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
@@ -32,6 +37,9 @@ class CarWashStation:
 
         return washing_price
 
-    def rate_service(self, rating: int):
-         self.average_rating = round((self.average_rating * self.count_of_ratings + rating) / (self.count_of_ratings + 1), 1)
-         self.count_of_ratings += 1
+    def rate_service(self, rating: int) -> None:
+        self.average_rating = round(
+            (self.average_rating
+             * self.count_of_ratings + rating)
+            / (self.count_of_ratings + 1), 1)
+        self.count_of_ratings += 1
